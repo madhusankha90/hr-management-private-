9d382e18f92628c269700501ac7f102b05ab8044
madhusankha90 <madhusankha.networking@gmail.com>
2024-12-14 04:06:56 +0530

    photo upload function create

--- backend/controllers/profilePicController.js
+++ backend/controllers/profilePicController.js
@@ -1,10 +1,10 @@
 const ProfilePicture = require('../models/myInfoModels/ProfilePicModel');
 const fs = require('fs');
 const path = require('path');
-const { upload } = require('')
 
 const createProfilePic = async( req, res) => {
     const employeeId = req.user?.employeeId || req.headers["employee-id"];
+    const profilePic = req.file;
     try {
         if (!employeeId) {
             return res.status(400).json({
@@ -19,15 +19,17 @@ const createProfilePic = async( req, res) => {
                 message: "No file Uploaded"
             })
         }
+        const profilePicPath = profilePic.path;
 
-        const createdProfilePic = new ProfilePicture({
+        const createProPic = new ProfilePicture({
             employeeId,
-            profilePic
+            profilePic: profilePicPath,
         })
-        await createdProfilePic.save();
+        await createProPic.save();
         res.status(201).json({
-            success: false,
-            message: 'Profile Picture saved successfully'
+            success: true,
+            message: 'Profile Picture saved successfully',
+            profilePic: createProPic.profilePic
         })
 
     } catch (error) {
@@ -41,6 +43,7 @@ const createProfilePic = async( req, res) => {
 
 const updateProfilePic = async (req, res) => {
     const employeeId = req.user?.employeeId || req.headers["employee-id"];
+
     try {
         if (!employeeId) {
             return res.status(400).json({
@@ -48,8 +51,84 @@ const updateProfilePic = async (req, res) => {
             })
         }
 
-    } catch (error) {
+        if (!req.file) {
+            return res.status(400).json({
+                success: false,
+                message: "No file Uploaded"
+            })
+        }
+
+        const existingProPic = await ProfilePicture.findOne(
+            { employeeId }
+        )
         
+        if (!existingProPic) {
+            return res.status(404).json({
+                success: false,
+                message: 'Profile Picture not found'
+            })
+        }
+
+        if (existingProPic.profilePic) {
+            const existingFilePath = path.join(
+                __dirname,
+                "..",
+                "uploads",
+                "profilePics",
+                existingProPic.profilePic
+            );
+
+            if (fs.existsSync(existingFilePath)) {
+                fs.unlinkSync(existingFilePath);
+            }
+        }
+        
+        existingProPic.profilePic = req.file.filename;
+        await existingProPic.save();
+        res.status(200).json({
+            success: true,
+            message: 'Profile Picture updated successfully',
+            profilePic: existingProPic.profilePic
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Internal server Error in update Profile Picture",
+            error: error.message
+        })
     }
 }
 
+const getProfilePic = async (req, res) => {
+    const employeeId = req.user?.employeeId || req.headers["employee-id"];
+    try {
+        if (!employeeId) {
+            return res.status(400).json({
+                success: false,
+                message: 'EmployeeId required to get Profile picture'
+            })
+        }
+        const getProPic = await ProfilePicture.findOne({employeeId});
+        if (!gotProfilePic) {
+            return res.status(404).json({
+                success: false,
+                message: "Profile Picture not found"
+            })
+        }
+        res.json({
+            profilePic: getProPic.profilePic
+            ? `${req.protocol}://${req.get('host')}/${uploadDir}/${getProPic.profilePic}`
+            : null
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Internal server error in get Profile Picture",
+            error: error.message
+        })
+    }
+}
+
+module.exports = {createProfilePic, updateProfilePic, getProfilePic}
--- backend/routes/uploadRouter.js
+++ backend/routes/uploadRouter.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { uploadFile } = require('../controllers/fileController');
 const { upload } = require('../middleware/uploadMiddleware');
+const { createProfilePic, updateProfilePic } = require('../controllers/profilePicController');
 const router = express.Router();
 
-router.post('/upload-profilePic', upload.single('photo'));
+router.post('/create-profilePic', upload.single('photo'), createProfilePic);
+router.put('/update-profilePic', upload.single('photo'), updateProfilePic);
 router.get('/get-pofilePic')
 
 router.post('/attachment',uploadFile);
